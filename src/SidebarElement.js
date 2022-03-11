604fda41a34acfd03b9601696f89907d4d54cfb5
Domiziano Scarcelli <scarcelli.d@outlook.com>
2022-03-11 20:03:01 +0100

    Added css modules

--- src/SidebarElement.js
+++ src/SidebarElement.js
@@ -1,37 +1,17 @@
 import React from "react"
+import styles from "./SidebarElement.module.css"
 
 export default function SidebarElement(props) {
-	const icon = {
+	const classNames = require("classnames")
+
+	const iconImage = {
 		backgroundImage: `url(${props.iconUrl})`,
-		width: "20px",
-		height: "20px",
-		backgroundPosition: "center",
-		backgroundSize: "contain",
-		backgroundRepeat: "no-repeat",
-	}
-
-	const container = {
-		cursor: "pointer",
-		backgroundColor: props.selected ? "var(--light)" : "var(--purple)",
-		display: "flex",
-		marginLeft: props.selected ? "1rem" : "",
-		flexDirection: "row",
-		padding: props.selected ? "0.5rem 0 0.5rem 1rem" : "1rem 0 0 2rem",
-		height: "fit-content",
-		borderRadius: props.first ? "0 2rem 0 0" : props.selected ? "12rem" : "",
-	}
-
-	const element = {
-		paddingLeft: "10px",
-		fontSize: "22px",
-		fontWeight: "bold",
-		color: props.selected ? "var(--dark-grey)" : "var(--light)",
 	}
 
 	return (
-		<div style={container}>
-			<div style={icon}></div>
-			<div style={element}>{props.name}</div>
+		<div className={classNames(styles.container, { [styles.first]: props.first }, { [styles.selected]: props.selected })}>
+			<div style={iconImage} className={styles.icon}></div>
+			<div className={classNames(styles.element, { [styles.selectedElement]: props.selected })}>{props.name}</div>
 		</div>
 	)
 }
