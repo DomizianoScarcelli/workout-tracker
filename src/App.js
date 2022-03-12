311c6ada52cfdd285f349a007f0c3fded021676e
Domiziano Scarcelli <scarcelli.d@outlook.com>
2022-03-12 07:31:40 +0100

    Modified routes

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
 		<Router>
 			<Routes>
 				<Route index element={<Sidebar selected={"Home"} />} />
+				<Route path="/home" element={<Sidebar selected={"Home"} />} />
 				<Route path="/my-workouts" element={<Sidebar selected={"My Workouts"} />} />
 				<Route path="/analytics" element={<Sidebar selected={"Analytics"} />} />
 				<Route path="/settings" element={<Sidebar selected={"Settings"} />} />
--- src/components/SidebarElement.js
+++ src/components/SidebarElement.js
@@ -1,15 +1,26 @@
 import React from "react"
 import styles from "./SidebarElement.module.css"
+import { useNavigate } from "react-router-dom"
 
 export default function SidebarElement(props) {
 	const classNames = require("classnames")
+	const navigate = useNavigate()
 
 	const iconImage = {
 		backgroundImage: `url(${props.iconUrl})`,
 	}
 
+	const parseUrl = (string) => {
+		return "/" + string.toLowerCase().replace(" ", "-")
+	}
+
 	return (
-		<div className={classNames(styles.container, { [styles.after]: props.first }, { [styles.selected]: props.selected }, { [styles.before]: props.before })}>
+		<div
+			className={classNames(styles.container, { [styles.after]: props.first }, { [styles.selected]: props.selected }, { [styles.before]: props.before })}
+			onClick={() => {
+				navigate(parseUrl(props.name), { replace: true })
+			}}
+		>
 			<div style={iconImage} className={styles.icon}></div>
 			<div className={classNames(styles.element, { [styles.selectedElement]: props.selected })}>{props.name}</div>
 		</div>
