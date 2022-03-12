97af765ef3842cf31de938a6e462c2ff75816773
Domiziano Scarcelli <scarcelli.d@outlook.com>
2022-03-12 10:28:33 +0100

    Updated folder structure

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
-import Sidebar from "./components/Sidebar"
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import Sidebar from "./components/Sidebar/Sidebar"
+import { Routes, Route } from "react-router-dom"
 
 function App() {
 	return (
-		<Router>
-			<Routes>
-				<Route index element={<Sidebar selected={"home"} />} />
-				<Route path="/home" element={<Sidebar selected={"home"} />} />
-				<Route path="/my-workouts" element={<Sidebar selected={"my-workouts"} />} />
-				<Route path="/analytics" element={<Sidebar selected={"analytics"} />} />
-				<Route path="/settings" element={<Sidebar selected={"settings"} />} />
-			</Routes>
-		</Router>
+		<Routes>
+			<Route index element={<Sidebar selected={"home"} />} />
+			<Route path="/home" element={<Sidebar selected={"home"} />} />
+			<Route path="/my-workouts" element={<Sidebar selected={"my-workouts"} />} />
+			<Route path="/analytics" element={<Sidebar selected={"analytics"} />} />
+			<Route path="/settings" element={<Sidebar selected={"settings"} />} />
+		</Routes>
 	)
 }
 
---
+++ src/components/Sidebar/Sidebar.js
@@ -0,0 +1,22 @@
+import useRenderNavigation from "../../hooks/useRenderNavigation"
+import styles from "./Sidebar.module.css"
+
+function Sidebar(props) {
+	const { firstHalf, secondHalf } = useRenderNavigation(props.selected)
+
+	return (
+		<>
+			<div className={styles.sidebar}>
+				<div className={styles.topHalfSidebar}>
+					<div className={styles.profileIcon}></div>
+					<div className={styles.welcomeMessage}>Welcome back, Domiziano</div>
+					<div className={styles.navContainer}>{firstHalf.map((element) => element)}</div>
+				</div>
+				<div className={styles.navContainer}>{secondHalf.map((element) => element)}</div>
+				<div className={styles.bottomHalfSidebar}></div>
+			</div>
+		</>
+	)
+}
+
+export default Sidebar
