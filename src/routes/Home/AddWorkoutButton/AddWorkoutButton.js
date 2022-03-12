b20fcb394fac4f0833998f48027a88fe6b6ba5b3
Domiziano Scarcelli <scarcelli.d@outlook.com>
2022-03-12 14:48:47 +0100

    Created add workout button

--- src/routes/Home/AddWorkoutButton/AddWorkoutButton.js
+++ src/routes/Home/AddWorkoutButton/AddWorkoutButton.js
@@ -1,6 +1,17 @@
 import React from "react"
 import styles from "./AddWorkoutButton.module.css"
+import { motion } from "framer-motion"
 
 export default function AddWorkoutButton() {
-	return <div>AddWorkoutButton</div>
+	return (
+		<motion.div
+			whileHover={{
+				scale: 1.1,
+			}}
+			className={styles.container}
+		>
+			<div className={styles.text}>ADD WORKOUT</div>
+			<div className={styles.icon}>+</div>
+		</motion.div>
+	)
 }
--- src/routes/Home/Home.js
+++ src/routes/Home/Home.js
@@ -2,12 +2,18 @@ import React from "react"
 import DaySelector from "../../components/DaySelector/DaySelector"
 import Sidebar from "../../components/Sidebar/Sidebar"
 import styles from "./Home.module.css"
+import AddWorkoutButton from "./AddWorkoutButton/AddWorkoutButton"
 
 export default function Home() {
 	return (
 		<div className={styles.container}>
 			<Sidebar selected={"home"} />
-			<DaySelector />
+			<div className={styles.flexCol}>
+				<DaySelector />
+				<div className={styles.flexRow}>
+					<AddWorkoutButton />
+				</div>
+			</div>
 		</div>
 	)
 }
