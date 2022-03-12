7d80cb7cdecf6f7f7e4b9deb3f56222be63668b1
Domiziano Scarcelli <scarcelli.d@outlook.com>
2022-03-12 07:21:30 +0100

    Added react router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import Sidebar from "./components/Sidebar"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 
 function App() {
 	return (
-		<>
-			<Sidebar selected={"home"} />
-		</>
+		<Router>
+			<Routes>
+				<Route index element={<Sidebar selected={"Home"} />} />
+				<Route path="/my-workouts" element={<Sidebar selected={"My Workouts"} />} />
+				<Route path="/analytics" element={<Sidebar selected={"Analytics"} />} />
+				<Route path="/settings" element={<Sidebar selected={"Settings"} />} />
+			</Routes>
+		</Router>
 	)
 }
 
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -2,38 +2,30 @@ import styles from "./Sidebar.module.css"
 import SidebarElement from "./SidebarElement"
 
 function Sidebar(props) {
-	const navArray = ["home", "my-workouts", "analytics", "settings"]
+	const navArray = ["Home", "My Workouts", "Analytics", "Settings"]
 
 	const renderNavigation = () => {
 		let firstHalf = []
 		let secondHalf = []
-		const navString = props.selected.toLowerCase().replace(" ", "-")
-		const index = navArray.indexOf(navString)
+		const index = navArray.indexOf(props.selected)
 		for (let i = 0; i < index; i++) {
-			const name = parseString(navArray[i])
 			if (i === index - 1) {
-				firstHalf.push(<SidebarElement name={name} before={true} icon={navArray[i]} />)
+				firstHalf.push(<SidebarElement name={navArray[i]} before={true} icon={navArray[i]} />)
 			} else {
-				firstHalf.push(<SidebarElement name={name} icon={navArray[i]} />)
+				firstHalf.push(<SidebarElement name={navArray[i]} icon={navArray[i]} />)
 			}
 		}
 		for (let i = index; i < navArray.length; i++) {
-			const name = parseString(navArray[i])
 			if (i === index) {
-				secondHalf.push(<SidebarElement name={name} selected={true} icon={navArray[i]} />)
+				secondHalf.push(<SidebarElement name={navArray[i]} selected={true} icon={navArray[i]} />)
 			} else if (i === index + 1) {
-				secondHalf.push(<SidebarElement name={name} first={true} icon={navArray[i]} />)
+				secondHalf.push(<SidebarElement name={navArray[i]} first={true} icon={navArray[i]} />)
 			} else {
-				secondHalf.push(<SidebarElement name={name} />)
+				secondHalf.push(<SidebarElement name={navArray[i]} />)
 			}
 		}
 		console.log([firstHalf, secondHalf])
 		return [firstHalf, secondHalf]
-	}
-
-	const parseString = (string) => {
-		const result = string.replace("-", " ")
-		return result.charAt(0).toUpperCase() + result.slice(1)
 	}
 
 	return (
