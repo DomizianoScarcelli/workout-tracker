305aec1e1d274b208c9d829345ce7b9c14291e7e
Domiziano Scarcelli <scarcelli.d@outlook.com>
2022-03-14 23:14:37 +0100

    Added home page chart

--- src/routes/Home/FrequencyChart/FrequencyChart.js
+++ src/routes/Home/FrequencyChart/FrequencyChart.js
@@ -2,5 +2,51 @@ import React from "react"
 import styles from "./FrequencyChart.module.css"
 
 export default function FrequencyChart() {
-	return <div>FrequencyChart</div>
+	const workouts = [
+		{
+			minutes: 45,
+			day: "Tue 1",
+		},
+		{
+			minutes: 45,
+			day: "Tue 1",
+		},
+		{
+			minutes: 45,
+			day: "Tue 1",
+		},
+		{
+			minutes: 45,
+			day: "Tue 1",
+		},
+		{
+			minutes: 45,
+			day: "Tue 1",
+		},
+		{
+			minutes: 45,
+			day: "Tue 1",
+		},
+		{
+			minutes: 45,
+			day: "Tue 1",
+		},
+	]
+
+	return (
+		<div className={styles.container}>
+			<div className={styles.label}>Workouts</div>
+			<div className={styles.horizontalContainer}>
+				{workouts.map((workout, i) => {
+					return (
+						<div className={styles.infoContainer}>
+							<div className={styles.minutes}>{workout.minutes} min</div>
+							<div className={styles.chart}></div>
+							<div className={styles.day + " " + (i === 3 ? styles.selected : "")}>{workout.day}</div>
+						</div>
+					)
+				})}
+			</div>
+		</div>
+	)
 }
