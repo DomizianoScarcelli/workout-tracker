9ee39d12b0d55546e4ac53771de89dd4f9bf05f2
Domiziano Scarcelli <scarcelli.d@outlook.com>
2022-03-28 16:58:49 +0200

    Solved a width bug inside My Workout page

--- client/src/hooks/useQuery.js
+++ client/src/hooks/useQuery.js
@@ -3,6 +3,5 @@ import { useLocation } from "react-router-dom"
 
 export const useQuery = () => {
 	const { search } = useLocation()
-	console.log(search)
 	return React.useMemo(() => new URLSearchParams(search), [search])
 }
