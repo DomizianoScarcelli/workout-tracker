45ce8102db4a07218d87236fe95f096cc0cf7de3
Domiziano Scarcelli <scarcelli.domiziano@gmail.com>
2022-03-29 22:12:50 +0200

    Toggle navbar in mobile

--- client/src/components/Sidebar/Sidebar.js
+++ client/src/components/Sidebar/Sidebar.js
@@ -1,21 +1,48 @@
 import useRenderNavigation from "./useRenderNavigation"
 import styles from "./Sidebar.module.css"
+import HamburgerMenu from "../HamburgerMenu/HamburgerMenu"
+import { useState } from "react"
+import { useMediaQuery } from "../../hooks/useMediaQuery"
+import { motion } from "framer-motion"
 
 function Sidebar(props) {
 	const [firstHalf, secondHalf] = useRenderNavigation(props.selected)
+	const [isActive, setActive] = useState(false)
+	const isSmall = useMediaQuery("(max-width: 700px)")
 
+	const navbarVariants = {
+		toggle: {
+			height: isActive ? "fit-content" : isSmall ? "0px" : "100vw",
+			padding: isActive ? "10px" : "0px",
+		},
+	}
+
+	const navbarContentVariants = {
+		toggle: {
+			visibility: isActive ? "visible" : isSmall ? "hidden" : "visible",
+		},
+	}
 	return (
 		<>
-			<div className={styles.toggle}></div>
-			<div className={styles.sidebar}>
-				<div className={styles.topHalfSidebar}>
+			<HamburgerMenu
+				isActive={isActive}
+				toggleActive={() => {
+					setActive(!isActive)
+				}}
+			/>
+			<motion.div variants={navbarVariants} animate="toggle" className={styles.sidebar}>
+				<motion.div variants={navbarContentVariants} animate="toggle" className={styles.topHalfSidebar}>
 					<div className={styles.profileIcon}></div>
 					<div className={styles.welcomeMessage}>Welcome back, Domiziano</div>
-					<div className={styles.navContainer}>{firstHalf.map((element) => element)}</div>
-				</div>
-				<div className={styles.navContainer}>{secondHalf.map((element) => element)}</div>
+					<motion.div variants={navbarContentVariants} animate="toggle" className={styles.navContainer}>
+						{firstHalf.map((element) => element)}
+					</motion.div>
+				</motion.div>
+				<motion.div variants={navbarContentVariants} animate="toggle" className={styles.navContainer}>
+					{secondHalf.map((element) => element)}
+				</motion.div>
 				<div className={styles.bottomHalfSidebar}></div>
-			</div>
+			</motion.div>
 		</>
 	)
 }
