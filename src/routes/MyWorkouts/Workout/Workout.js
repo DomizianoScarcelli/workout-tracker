a1c3e01a7c856fc7c3070024df8d30191ab15dad
Domiziano Scarcelli <scarcelli.domiziano@gmail.com>
2022-04-08 19:31:45 +0200

    Design to delete a saved workout

--- src/routes/MyWorkouts/Workout/Workout.js
+++ src/routes/MyWorkouts/Workout/Workout.js
@@ -2,13 +2,20 @@ import React from "react"
 import styles from "./Workout.module.css"
 import { motion } from "framer-motion"
 import { useNavigate } from "react-router-dom"
+import { useState } from "react"
 
 export default function Workout(props) {
 	const navigate = useNavigate()
 
+	const [hovering, setHovering] = useState(false)
+
 	const openWorkoutEditor = () => {
 		localStorage.setItem("exercises", JSON.stringify(props.exercises))
 		navigate("new-workout")
+	}
+
+	const deleteSavedWorkout = () => {
+		//TODO: Delete the current saved workout from the DB
 	}
 
 	return (
@@ -17,8 +24,23 @@ export default function Workout(props) {
 			whileHover={{
 				scale: 1.1,
 			}}
+			onHoverStart={() => {
+				setHovering(true)
+			}}
+			onHoverEnd={() => {
+				setHovering(false)
+			}}
 			className={styles.container}
 		>
+			{hovering && (
+				<motion.div
+					whileHover={{
+						scale: 1.8,
+					}}
+					className={styles.closeButton}
+					onClick={deleteSavedWorkout}
+				></motion.div>
+			)}
 			<div>
 				<div className={styles.name}>{props.name}</div>
 				<div className={styles.exercises}>
