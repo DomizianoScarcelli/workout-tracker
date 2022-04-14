a603c3af1608b1fe2abf25047236ff3291267bfd
Domiziano Scarcelli <scarcelli.domiziano@gmail.com>
2022-04-14 12:27:41 +0200

    User can see the workout sorted by date in the history

--- src/routes/History/History.js
+++ src/routes/History/History.js
@@ -4,13 +4,21 @@ import Sidebar from "../../components/Sidebar/Sidebar"
 import { useState, useEffect } from "react"
 import Workout from "../MyWorkouts/Workout/Workout"
 import axios from "axios"
+import moment from "moment"
 
 export default function History() {
 	const [history, setHistory] = useState([])
+	const [workoutDays, setWorkoutDays] = useState([])
 
 	const getHistory = async (startDate, endDate) => {
 		const username = "DovivoD"
 		const res = await axios.get(`http://localhost:8080/sessions/${username}/workouts?startDate=${startDate}&endDate=${endDate}`)
+		let tempWorkoutDays = []
+		for (let workout of res.data) {
+			const date = moment(workout.date).format("MMMM DD YYYY")
+			if (!tempWorkoutDays.includes(date)) tempWorkoutDays.push(date)
+		}
+		setWorkoutDays(tempWorkoutDays)
 		setHistory(res.data)
 	}
 
@@ -36,9 +44,25 @@ export default function History() {
 					<div className={styles.headerIcon}></div>
 					<div className={styles.headerTitle}>History</div>
 				</div>
-				{history.map((workout) => (
-					<Workout exercises={workout.exercises} name={""} id={workout["_id"]} duration={workout.duration} removeWorkout={() => removeWorkoutFromHistory(workout["_id"])} />
-				))}
+				{workoutDays.map((day) => {
+					return (
+						<>
+							<div className={styles.date}>{moment(day).format("MMMM DD YYYY")}</div>
+							{history.map(
+								(workout) =>
+									moment(workout.date).isSame(moment(day), "day") && (
+										<Workout
+											exercises={workout.exercises}
+											name={""}
+											id={workout["_id"]}
+											duration={workout.duration}
+											removeWorkout={() => removeWorkoutFromHistory(workout["_id"])}
+										/>
+									)
+							)}
+						</>
+					)
+				})}
 			</div>
 		</div>
 	)
